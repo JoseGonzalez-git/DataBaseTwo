a8781ba00c5c58e761b5a3474cbe5d6c55df4a16
JoseGonzalez-git <JoseGonzalez-git@github.com>
2021-11-04 14:30:04 -0500

    sql exercice 5

---
+++ Actividad001/mysql5.sql
@@ -0,0 +1,217 @@
+CREATE TABLE personas (
+  identificacion INTEGER UNSIGNED  NOT NULL   AUTO_INCREMENT,
+  nom1 VARCHAR(20)  NOT NULL  ,
+  nom2 VARCHAR(20)  NOT NULL  ,
+  apll1 VARCHAR(35)  NOT NULL  ,
+  apll2 VARCHAR(35)  NOT NULL  ,
+  fnac DATE  NOT NULL  ,
+  genero VARCHAR(10)  NOT NULL  ,
+  contacto VARCHAR(45)  NOT NULL    ,
+PRIMARY KEY(identificacion));
+
+
+
+CREATE TABLE categorias (
+  idcategorias INTEGER UNSIGNED  NOT NULL   AUTO_INCREMENT,
+  nombre VARCHAR(45)  NOT NULL    ,
+PRIMARY KEY(idcategorias));
+
+
+
+CREATE TABLE paises (
+  idpaises INTEGER UNSIGNED  NOT NULL   AUTO_INCREMENT,
+  nombre INTEGER UNSIGNED  NOT NULL    ,
+PRIMARY KEY(idpaises));
+
+
+
+CREATE TABLE locales (
+  idlocales INTEGER UNSIGNED  NOT NULL   AUTO_INCREMENT,
+  nombre VARCHAR(45)  NOT NULL  ,
+  contacto VARCHAR(45)  NOT NULL  ,
+  correo VARCHAR(255)  NOT NULL  ,
+  direccion VARCHAR(45)  NOT NULL  ,
+  zipcode VARCHAR(20))  NOT NULL    ,
+PRIMARY KEY(idlocales));
+
+
+
+CREATE TABLE clientes (
+  idclientes INTEGER UNSIGNED  NOT NULL   AUTO_INCREMENT,
+  personas_identificacion INTEGER UNSIGNED  NOT NULL    ,
+PRIMARY KEY(idclientes)  ,
+INDEX clientes_FKIndex1(personas_identificacion),
+  FOREIGN KEY(personas_identificacion)
+    REFERENCES personas(identificacion)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE);
+
+
+
+CREATE TABLE actores (
+  idactores INTEGER UNSIGNED  NOT NULL   AUTO_INCREMENT,
+  personas_identificacion INTEGER UNSIGNED  NOT NULL    ,
+PRIMARY KEY(idactores)  ,
+INDEX actores_FKIndex1(personas_identificacion),
+  FOREIGN KEY(personas_identificacion)
+    REFERENCES personas(identificacion)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE);
+
+
+
+CREATE TABLE ciudades (
+  idciudades INTEGER UNSIGNED  NOT NULL   AUTO_INCREMENT,
+  paises_idpaises INTEGER UNSIGNED  NOT NULL  ,
+  nombre VARCHAR(45)  NOT NULL    ,
+PRIMARY KEY(idciudades)  ,
+INDEX ciudades_FKIndex1(paises_idpaises),
+  FOREIGN KEY(paises_idpaises)
+    REFERENCES paises(idpaises)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE);
+
+
+
+CREATE TABLE sucursales (
+  locales_idlocales INTEGER UNSIGNED  NOT NULL  ,
+  ciudades_idciudades INTEGER UNSIGNED  NOT NULL    ,
+PRIMARY KEY(locales_idlocales, ciudades_idciudades)  ,
+INDEX locales_has_ciudades_FKIndex1(locales_idlocales)  ,
+INDEX locales_has_ciudades_FKIndex2(ciudades_idciudades),
+  FOREIGN KEY(locales_idlocales)
+    REFERENCES locales(idlocales)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE,
+  FOREIGN KEY(ciudades_idciudades)
+    REFERENCES ciudades(idciudades)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE);
+
+
+
+CREATE TABLE empleados (
+  idempleados INTEGER UNSIGNED  NOT NULL   AUTO_INCREMENT,
+  locales_idlocales INTEGER UNSIGNED  NOT NULL  ,
+  personas_identificacion INTEGER UNSIGNED  NOT NULL    ,
+PRIMARY KEY(idempleados)  ,
+INDEX empleados_FKIndex1(personas_identificacion)  ,
+INDEX empleados_FKIndex2(locales_idlocales),
+  FOREIGN KEY(personas_identificacion)
+    REFERENCES personas(identificacion)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE,
+  FOREIGN KEY(locales_idlocales)
+    REFERENCES locales(idlocales)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE);
+
+
+
+CREATE TABLE peliculas (
+  idpeliculas INTEGER UNSIGNED  NOT NULL   AUTO_INCREMENT,
+  actores_idactores INTEGER UNSIGNED  NOT NULL  ,
+  nombre VARCHAR(45)  NOT NULL  ,
+  descripcion TEXT  NOT NULL  ,
+  duracion TIME  NOT NULL  ,
+  fecha_lanzamiento DATE  NOT NULL  ,
+  precio DOUBLE  NOT NULL    ,
+PRIMARY KEY(idpeliculas)  ,
+INDEX peliculas_FKIndex1(actores_idactores),
+  FOREIGN KEY(actores_idactores)
+    REFERENCES actores(idactores)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE);
+
+
+
+CREATE TABLE peliculas_tiene_categorias (
+  peliculas_idpeliculas INTEGER UNSIGNED  NOT NULL  ,
+  categorias_idcategorias INTEGER UNSIGNED  NOT NULL    ,
+PRIMARY KEY(peliculas_idpeliculas, categorias_idcategorias)  ,
+INDEX peliculas_has_categorias_FKIndex1(peliculas_idpeliculas)  ,
+INDEX peliculas_has_categorias_FKIndex2(categorias_idcategorias),
+  FOREIGN KEY(peliculas_idpeliculas)
+    REFERENCES peliculas(idpeliculas)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE,
+  FOREIGN KEY(categorias_idcategorias)
+    REFERENCES categorias(idcategorias)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE);
+
+
+
+CREATE TABLE pedidos (
+  idpedidos INTEGER UNSIGNED  NOT NULL   AUTO_INCREMENT,
+  clientes_idclientes INTEGER UNSIGNED  NOT NULL  ,
+  empleados_idempleados INTEGER UNSIGNED  NOT NULL  ,
+  fecha_entrega DATE  NULL    ,
+PRIMARY KEY(idpedidos)  ,
+INDEX pedidos_FKIndex1(empleados_idempleados)  ,
+INDEX pedidos_FKIndex2(clientes_idclientes),
+  FOREIGN KEY(empleados_idempleados)
+    REFERENCES empleados(idempleados)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE,
+  FOREIGN KEY(clientes_idclientes)
+    REFERENCES clientes(idclientes)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE);
+
+
+
+CREATE TABLE catalogo_de_peliculas (
+  locales_idlocales INTEGER UNSIGNED  NOT NULL  ,
+  peliculas_idpeliculas INTEGER UNSIGNED  NOT NULL  ,
+  cantidad INTEGER UNSIGNED  NOT NULL    ,
+PRIMARY KEY(locales_idlocales, peliculas_idpeliculas)  ,
+INDEX locales_has_peliculas_FKIndex1(locales_idlocales)  ,
+INDEX locales_has_peliculas_FKIndex2(peliculas_idpeliculas),
+  FOREIGN KEY(locales_idlocales)
+    REFERENCES locales(idlocales)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE,
+  FOREIGN KEY(peliculas_idpeliculas)
+    REFERENCES peliculas(idpeliculas)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE);
+
+
+
+CREATE TABLE catalogo_de_actores (
+  peliculas_idpeliculas INTEGER UNSIGNED  NOT NULL  ,
+  actores_idactores INTEGER UNSIGNED  NOT NULL    ,
+PRIMARY KEY(peliculas_idpeliculas, actores_idactores)  ,
+INDEX peliculas_has_actores_FKIndex1(peliculas_idpeliculas)  ,
+INDEX peliculas_has_actores_FKIndex2(actores_idactores),
+  FOREIGN KEY(peliculas_idpeliculas)
+    REFERENCES peliculas(idpeliculas)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE,
+  FOREIGN KEY(actores_idactores)
+    REFERENCES actores(idactores)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE);
+
+
+
+CREATE TABLE ejemplar_pelicula (
+  idejemplar_pelicula INTEGER UNSIGNED  NOT NULL   AUTO_INCREMENT,
+  pedidos_idpedidos INTEGER UNSIGNED  NOT NULL  ,
+  peliculas_idpeliculas INTEGER UNSIGNED  NOT NULL    ,
+PRIMARY KEY(idejemplar_pelicula)  ,
+INDEX ejemplar_pelicula_FKIndex1(peliculas_idpeliculas)  ,
+INDEX ejemplar_pelicula_FKIndex2(pedidos_idpedidos),
+  FOREIGN KEY(peliculas_idpeliculas)
+    REFERENCES peliculas(idpeliculas)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE,
+  FOREIGN KEY(pedidos_idpedidos)
+    REFERENCES pedidos(idpedidos)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE);
+
+
+
+
---
+++ Actividad001/psql5.sql
@@ -0,0 +1,272 @@
+CREATE TABLE personas (
+  identificacion SERIAL  NOT NULL ,
+  nom1 VARCHAR(20)   NOT NULL ,
+  nom2 VARCHAR(20)   NOT NULL ,
+  apll1 VARCHAR(35)   NOT NULL ,
+  apll2 VARCHAR(35)   NOT NULL ,
+  fnac DATE   NOT NULL ,
+  genero VARCHAR(10)   NOT NULL ,
+  contacto VARCHAR(45)   NOT NULL   ,
+PRIMARY KEY(identificacion));
+
+
+
+
+CREATE TABLE categorias (
+  idcategorias SERIAL  NOT NULL ,
+  nombre VARCHAR(45)   NOT NULL   ,
+PRIMARY KEY(idcategorias));
+
+
+
+
+CREATE TABLE paises (
+  idpaises SERIAL  NOT NULL ,
+  nombre INTEGER   NOT NULL   ,
+PRIMARY KEY(idpaises));
+
+
+
+
+CREATE TABLE locales (
+  idlocales SERIAL  NOT NULL ,
+  nombre VARCHAR(45)   NOT NULL ,
+  contacto VARCHAR(45)   NOT NULL ,
+  correo VARCHAR(255)   NOT NULL ,
+  direccion VARCHAR(45)   NOT NULL ,
+  zipcode VARCHAR(20))   NOT NULL   ,
+PRIMARY KEY(idlocales));
+
+
+
+
+CREATE TABLE clientes (
+  idclientes SERIAL  NOT NULL ,
+  personas_identificacion INTEGER   NOT NULL   ,
+PRIMARY KEY(idclientes)  ,
+  FOREIGN KEY(personas_identificacion)
+    REFERENCES personas(identificacion)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE);
+
+
+CREATE INDEX clientes_FKIndex1 ON clientes (personas_identificacion);
+
+
+CREATE INDEX IFK_son ON clientes (personas_identificacion);
+
+
+CREATE TABLE actores (
+  idactores SERIAL  NOT NULL ,
+  personas_identificacion INTEGER   NOT NULL   ,
+PRIMARY KEY(idactores)  ,
+  FOREIGN KEY(personas_identificacion)
+    REFERENCES personas(identificacion)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE);
+
+
+CREATE INDEX actores_FKIndex1 ON actores (personas_identificacion);
+
+
+CREATE INDEX IFK_pueden ser ON actores (personas_identificacion);
+
+
+CREATE TABLE ciudades (
+  idciudades SERIAL  NOT NULL ,
+  paises_idpaises INTEGER   NOT NULL ,
+  nombre VARCHAR(45)   NOT NULL   ,
+PRIMARY KEY(idciudades)  ,
+  FOREIGN KEY(paises_idpaises)
+    REFERENCES paises(idpaises)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE);
+
+
+CREATE INDEX ciudades_FKIndex1 ON ciudades (paises_idpaises);
+
+
+CREATE INDEX IFK_por ON ciudades (paises_idpaises);
+
+
+CREATE TABLE sucursales (
+  locales_idlocales INTEGER   NOT NULL ,
+  ciudades_idciudades INTEGER   NOT NULL   ,
+PRIMARY KEY(locales_idlocales, ciudades_idciudades)    ,
+  FOREIGN KEY(locales_idlocales)
+    REFERENCES locales(idlocales)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE,
+  FOREIGN KEY(ciudades_idciudades)
+    REFERENCES ciudades(idciudades)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE);
+
+
+CREATE INDEX locales_has_ciudades_FKIndex1 ON sucursales (locales_idlocales);
+CREATE INDEX locales_has_ciudades_FKIndex2 ON sucursales (ciudades_idciudades);
+
+
+CREATE INDEX IFK_tiene ON sucursales (locales_idlocales);
+CREATE INDEX IFK_en ON sucursales (ciudades_idciudades);
+
+
+CREATE TABLE empleados (
+  idempleados SERIAL  NOT NULL ,
+  locales_idlocales INTEGER   NOT NULL ,
+  personas_identificacion INTEGER   NOT NULL   ,
+PRIMARY KEY(idempleados)    ,
+  FOREIGN KEY(personas_identificacion)
+    REFERENCES personas(identificacion)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE,
+  FOREIGN KEY(locales_idlocales)
+    REFERENCES locales(idlocales)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE);
+
+
+CREATE INDEX empleados_FKIndex1 ON empleados (personas_identificacion);
+CREATE INDEX empleados_FKIndex2 ON empleados (locales_idlocales);
+
+
+CREATE INDEX IFK_aplican como ON empleados (personas_identificacion);
+CREATE INDEX IFK_trabajan ON empleados (locales_idlocales);
+
+
+CREATE TABLE peliculas (
+  idpeliculas SERIAL  NOT NULL ,
+  actores_idactores INTEGER   NOT NULL ,
+  nombre VARCHAR(45)   NOT NULL ,
+  descripcion TEXT   NOT NULL ,
+  duracion TIME   NOT NULL ,
+  fecha_lanzamiento DATE   NOT NULL ,
+  precio DOUBLE   NOT NULL   ,
+PRIMARY KEY(idpeliculas)  ,
+  FOREIGN KEY(actores_idactores)
+    REFERENCES actores(idactores)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE);
+
+
+CREATE INDEX peliculas_FKIndex1 ON peliculas (actores_idactores);
+
+
+CREATE INDEX IFK_papel_principal ON peliculas (actores_idactores);
+
+
+CREATE TABLE peliculas_tiene_categorias (
+  peliculas_idpeliculas INTEGER   NOT NULL ,
+  categorias_idcategorias INTEGER   NOT NULL   ,
+PRIMARY KEY(peliculas_idpeliculas, categorias_idcategorias)    ,
+  FOREIGN KEY(peliculas_idpeliculas)
+    REFERENCES peliculas(idpeliculas)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE,
+  FOREIGN KEY(categorias_idcategorias)
+    REFERENCES categorias(idcategorias)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE);
+
+
+CREATE INDEX peliculas_has_categorias_FKIndex1 ON peliculas_tiene_categorias (peliculas_idpeliculas);
+CREATE INDEX peliculas_has_categorias_FKIndex2 ON peliculas_tiene_categorias (categorias_idcategorias);
+
+
+CREATE INDEX IFK_se clasifica ON peliculas_tiene_categorias (peliculas_idpeliculas);
+CREATE INDEX IFK_tiene ON peliculas_tiene_categorias (categorias_idcategorias);
+
+
+CREATE TABLE pedidos (
+  idpedidos SERIAL  NOT NULL ,
+  clientes_idclientes INTEGER   NOT NULL ,
+  empleados_idempleados INTEGER   NOT NULL ,
+  fecha_entrega DATE      ,
+PRIMARY KEY(idpedidos)    ,
+  FOREIGN KEY(empleados_idempleados)
+    REFERENCES empleados(idempleados)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE,
+  FOREIGN KEY(clientes_idclientes)
+    REFERENCES clientes(idclientes)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE);
+
+
+CREATE INDEX pedidos_FKIndex1 ON pedidos (empleados_idempleados);
+CREATE INDEX pedidos_FKIndex2 ON pedidos (clientes_idclientes);
+
+
+CREATE INDEX IFK_atienden ON pedidos (empleados_idempleados);
+CREATE INDEX IFK_realiza ON pedidos (clientes_idclientes);
+
+
+CREATE TABLE catalogo_de_peliculas (
+  locales_idlocales INTEGER   NOT NULL ,
+  peliculas_idpeliculas INTEGER   NOT NULL ,
+  cantidad INTEGER   NOT NULL   ,
+PRIMARY KEY(locales_idlocales, peliculas_idpeliculas)    ,
+  FOREIGN KEY(locales_idlocales)
+    REFERENCES locales(idlocales)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE,
+  FOREIGN KEY(peliculas_idpeliculas)
+    REFERENCES peliculas(idpeliculas)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE);
+
+
+CREATE INDEX locales_has_peliculas_FKIndex1 ON catalogo_de_peliculas (locales_idlocales);
+CREATE INDEX locales_has_peliculas_FKIndex2 ON catalogo_de_peliculas (peliculas_idpeliculas);
+
+
+CREATE INDEX IFK_gestiona ON catalogo_de_peliculas (locales_idlocales);
+CREATE INDEX IFK_se guardan ON catalogo_de_peliculas (peliculas_idpeliculas);
+
+
+CREATE TABLE catalogo_de_actores (
+  peliculas_idpeliculas INTEGER   NOT NULL ,
+  actores_idactores INTEGER   NOT NULL   ,
+PRIMARY KEY(peliculas_idpeliculas, actores_idactores)    ,
+  FOREIGN KEY(peliculas_idpeliculas)
+    REFERENCES peliculas(idpeliculas)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE,
+  FOREIGN KEY(actores_idactores)
+    REFERENCES actores(idactores)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE);
+
+
+CREATE INDEX peliculas_has_actores_FKIndex1 ON catalogo_de_actores (peliculas_idpeliculas);
+CREATE INDEX peliculas_has_actores_FKIndex2 ON catalogo_de_actores (actores_idactores);
+
+
+CREATE INDEX IFK_tiene ON catalogo_de_actores (peliculas_idpeliculas);
+CREATE INDEX IFK_participan ON catalogo_de_actores (actores_idactores);
+
+
+CREATE TABLE ejemplar_pelicula (
+  idejemplar_pelicula SERIAL  NOT NULL ,
+  pedidos_idpedidos INTEGER   NOT NULL ,
+  peliculas_idpeliculas INTEGER   NOT NULL   ,
+PRIMARY KEY(idejemplar_pelicula)    ,
+  FOREIGN KEY(peliculas_idpeliculas)
+    REFERENCES peliculas(idpeliculas)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE,
+  FOREIGN KEY(pedidos_idpedidos)
+    REFERENCES pedidos(idpedidos)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE);
+
+
+CREATE INDEX ejemplar_pelicula_FKIndex1 ON ejemplar_pelicula (peliculas_idpeliculas);
+CREATE INDEX ejemplar_pelicula_FKIndex2 ON ejemplar_pelicula (pedidos_idpedidos);
+
+
+CREATE INDEX IFK_asocia ON ejemplar_pelicula (peliculas_idpeliculas);
+CREATE INDEX IFK_registra ON ejemplar_pelicula (pedidos_idpedidos);
+
+
+
