fe054ca1ba10d408b208575e6c7119505edb6402
JoseGonzalez-git <JoseGonzalez-git@github.com>
2021-11-05 11:51:14 -0500

    MORE DATA BUT FALTA DATA

--- Actividad001/1/data.sql
+++ Actividad001/1/data.sql
@@ -8,7 +8,7 @@ VALUES (
         'Perez',
         '2000-01-01'
     ),
-(
+    (
         2144645,
         'C.C',
         'Jhoan',
@@ -16,8 +16,8 @@ VALUES (
         'Ramirez',
         'Arteaga',
         '2002-03-04'
-),
-(
+    ),
+    (
         514356,
         'C.C',
         'Luz',
@@ -25,8 +25,8 @@ VALUES (
         'Estrella',
         'Gonzalez',
         '2001-08-20'
-),
-(
+    ),
+    (
         614356,
         'C.C',
         'Maria',
@@ -34,17 +34,17 @@ VALUES (
         'Pereira',
         'Lopez',
         '2003-08-20'
-),
-(
+    ),
+    (
         714356,
         'C.C',
         'Adalia',
-        'De Jesus',
+        'Melissa',
         'Mestra',
-        'Arguelles',
+        'Martinez',
         '2000-08-20'
-),
-(
+    ),
+    (
         3215346,
         'C.C',
         'Luisa',
@@ -52,35 +52,30 @@ VALUES (
         'Velazquez',
         'Espitia',
         '2002-05-20'
-),
-( 
+    ),
+    (
         675443,
-        'T.I',
+        'C.C',
         'Juan',
         'Carlos',
         'Henriquez',
         'Peñanieto',
-);
-
-INSERT INTO paises (idpais, nombre_pais)
-VALUES (
-        1,
-        'Mexico'
-    ),
-(
-        2,
-        'Colombia'
-    ),
-(
-        3,
-        'Venezuela'
-    ),
-(
-        4,
-        'Peru'
     );
-
-INSERT INTO series (idseries, nombre_serie, fecha_estreno, emicion, duracion_episodio, descripcion, mezcla_sonido, formato_relacion_aspecto)
+INSERT INTO paises (idpais, nombre_pais)
+VALUES (1, 'Mexico'),
+    (2, 'Colombia'),
+    (3, 'Venezuela'),
+    (4, 'Peru');
+INSERT INTO series (
+        idseries,
+        nombre_serie,
+        fecha_estreno,
+        emicion,
+        duracion_episodio,
+        descripcion,
+        mezcla_sonido,
+        formato_relacion_aspecto
+    )
 VALUES (
         1,
         'The Walking Dead',
@@ -90,8 +85,8 @@ VALUES (
         'Una serie de ciencia ficción que narra la historia de una población de una región del norte de América, que se enfrenta a una pandemia que amenaza con la supervivencia de la humanidad.',
         'TensionSong',
         '16:9'
-),
-(
+    ),
+    (
         2,
         'The Big Bang Theory',
         '2007-09-24',
@@ -100,8 +95,8 @@ VALUES (
         'Una serie de televisión que narra la vida de una familia de quienes trabajan en una empresa de informática, donde la mayoría de sus miembros son estudiantes universitarios.',
         'AccionSong',
         '16:9'
-),
-(
+    ),
+    (
         3,
         'Los Simpsons',
         '1989-12-17',
@@ -110,7 +105,8 @@ VALUES (
         'Son una familia normal estadounidense que vive en Springfield, una ciudad ficticia de la America Media.',
         'IntroSong',
         '3:4'
-),(
+    ),
+    (
         4,
         'Arcane',
         '2021-09-12',
@@ -119,65 +115,56 @@ VALUES (
         'Arcane está ambientado en el universo del exitoso MOBA de Riot Games, y cuenta la historia de dos de las hermanas más populares de su universo: Jinx y Vi.',
         'EpicSong',
         '16:9'
-);
-
-INSERT INTO categorias (idcategorias, nombre_categoria)
-VALUES (
-        1,
-        'Accion'
-    ),
-(
-        2,
-        'Comedia'
-    ),
-(
-        3,
-        'Drama'
-    ),
-(
-        4,
-        'Terror'
-    ),
-(
-        5,
-        'Fantasia'
-    ),
-(
-        6,
-        'Anime'
     );
-
-INSERT INTO usuario (idusuario, persona_nid, nombre_usuario, correo_electronico, contrasena)
+INSERT INTO categorias (idcategorias, nombre_categoria)
+VALUES (1, 'Accion'),
+    (2, 'Comedia'),
+    (3, 'Drama'),
+    (4, 'Terror'),
+    (5, 'Fantasia'),
+    (6, 'Anime');
+INSERT INTO usuario (
+        idusuario,
+        persona_nid,
+        nombre_usuario,
+        correo_electronico,
+        contrasena
+    )
 VALUES (
         1,
         132545,
         'juan',
         'juachito@gmail.com',
         'Craken'
-),
-(
+    ),
+    (
         2,
         2144645,
         'jhoan',
         'eljoalo@gmail.com',
         'surucutanga'
-),
-(
+    ),
+    (
         3,
         514356,
         'luz',
         'labebecita@gmail.com',
         'bebelin'
-),
-(
+    ),
+    (
         4,
         614356,
         'maria',
         'mariela@gmail.com',
         'deusamadeus'
-);
-
-INSERT INTO premios_serie (idpremios, series_idseries, nombre_premio, fecha_premio, gano)
+    );
+INSERT INTO premios_serie (
+        idpremios,
+        series_idseries,
+        nombre_premio,
+        fecha_premio,
+        gano
+    )
 VALUES (
         1,
         1,
@@ -185,166 +172,132 @@ VALUES (
         '2010-10-31',
         TRUE
     ),
-(
+    (
         2,
         2,
         'Mejor serie de terror',
         '2010-10-31',
         TRUE
     ),
-(
+    (
         3,
         3,
         'Mejor serie de fantasia',
         '2010-10-31',
         TRUE
     ),
-(
+    (
         4,
         4,
         'Mejor serie de anime',
         '2010-10-31',
         TRUE
     );
-
 INSERT INTO ciudades (idciudad, paises_idpais, nombre_ciudad)
-VALUES (
-        1,
-        2,
-        'Barranquilla'
-    ),
-(
-        2,
-        2,
-        'Sucre'
-    ),
-(
-        3,
-        1,
-        'Popotla'
-    ),
-(
-        4,
-        1,
-        'Ciudad de Mexico'
-    );
-
+VALUES 
+    (1,2,'Barranquilla'),
+    (2, 2, 'Coveñas'),
+    (3, 1, 'Popotla'),
+    (4,1,'Ciudad de Mexico');
 INSERT INTO actores (idactores, persona_nid)
-VALUES (
-        1,
-        614356
-    );
-
+VALUES (1, 614356),
+    (2, 2144645);
 INSERT INTO temporadas (n_temporada, series_idseries)
-VALUES (
-        1,
-        2
-    ),
-(
-        2,
-        3
-    ),
-(
-        3,
-        1
-    ),
-(
-        4,
-        1
-    );
-
+VALUES (1, 2),
+    (2, 3),
+    (3, 1),
+    (4, 1);
 INSERT INTO director (iddirector, persona_nid)
+VALUES (1, 2144645),
+    (2, 514356);
+INSERT INTO valoracion_escrita_serie (
+        idvaloracion_num,
+        usuario_idusuario,
+        series_idseries,
+        valoracion
+    )
+VALUES (1, 1, 1, 5),
+    (2, 2, 1, 4),
+    (3, 3, 1, 3),
+    (4, 4, 1, 2);
+INSERT INTO episodios (
+        n_episodios,
+        director_iddirector,
+        temporadas_n_temporada,
+        nombre_episodio,
+        descripcion,
+        valoracion_numerica,
+        valoracion_escrita,
+        fecha_publicacion
+    )
 VALUES (
         1,
-        2144645
+        1,
+        1,
+        1,
+        'Mosca',
+        'El primer episodio de la serie',
+        '2020-10-31'
     ),
     (
         2,
-        514356
-    );
-
-INSERT INTO valoracion_escrita_serie (idvaloracion_num, usuario_idusuario, series_idseries, valoracion)
-VALUES (
-    1,
-    1,
-    1,
-    5
-),
-(
-    2,
-    2,
-    1,
-    4
-),
-(
-    3,
-    3,
-    1,
-    3
-),
-(
-    4,
-    4,
-    1,
-    2
-);
-
-INSERT INTO episodios (n_episodios, director_iddirector, temporadas_n_temporada, nombre_episodio, descripcion, valoracion_numerica, valoracion_escrita, fecha_publicacion)
-VALUES (
-    1,
-    1,
-    1,
-    1,
-    'Mosca',
-    'El primer episodio de la serie',
-    '2020-10-31'
-),
-(
-    2,
-    2,
-    1,
-    1,
-    'Ozymandias',
-    'El primer episodio de la segunda temporada de la serie',
-    '2020-11-31'
-),
-(
-    3,
-    3,
-    2,
-    1,
-    'Estrellas en la noche',
-    'El primer episodio de la serie',
-    '2010-10-20'
-),
-(   4,
-    4,
-    2,
-    1,
-    'El rey leon',
-    'El primer episodio de la serie',
-    '2020-01-31'
-    );
-
-INSERT INTO categorias_has_series (categorias_idcategorias, series_idseries)
-VALUES (
+        2,
         1,
-        1
+        1,
+        'Ozymandias',
+        'El primer episodio de la segunda temporada de la serie',
+        '2020-11-31'
     ),
-(
-        2,
-        2
-    ),
-(
+    (
         3,
-        1
-    ),
-(
-        4,
-        4
-    ),
-(
+        3,
         2,
-        2
+        1,
+        'Estrellas en la noche',
+        'El primer episodio de la serie',
+        '2010-10-20'
+    ),
+    (
+        4,
+        4,
+        2,
+        1,
+        'El rey leon',
+        'El primer episodio de la serie',
+        '2020-01-31'
     );
+INSERT INTO categorias_has_series (categorias_idcategorias, series_idseries)
+VALUES (1, 1),
+    (2, 2),
+    (3, 1),
+    (4, 4),
+    (2, 2);
+INSERT INTO productores (idproductores, episodio_id_episodio, persona_nid)
+VALUES(1, 1, 675443),
+    (2, 2, 675443),
+    (3, 3, 3215346),
+    (4, 4, 3215346);
+INSERT INTO personajes (
+        idpersonajes,
+        episodio_id_episodio,
+        actores_idactores,
+        nombre_personaje
+    )
+VALUES(1, 1, 2, 'Jack Bauer'),
+    (2, 2, 1, 'Criss Smith'),
+    (3, 3, 2, 'Lisa Simpson'),
+    (4, 4, 1, 'Lee Sin');
+
+INSERT INTO lugar_rodaje (idlugar_rodaje, episodio_id_episodio, ciudades_idciudad, nombre_lugar)
+VALUES(1, 1, 1, 'Monumento Ventana al Mundo'),
+    (2, 2, 2, 'Frente al Mar'),
+    (3, 3, 1, 'Plaza de la Paz'),
+    (4, 4, 2, 'Base Militar');
+
+INSERT INTO premios_actores (idpremios_actores, actores_idactores, nombre_premio, fecha_entrega, gano)
+VALUES(1, 1, 'Mejor actor', '2021-10-31', TRUE),
+    (2, 2, 'Mejor actor', '2020-10-31', TRUE),
+    (3, 3, 'Mejor actor', '2022-10-31', FALSE),
+    (4, 4, 'Mejor actor', '2021-10-31', FALSE);
+
 
