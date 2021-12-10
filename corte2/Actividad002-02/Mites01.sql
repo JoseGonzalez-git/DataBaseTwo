338a4a6bb19e455e6afaafea70e31029f8c9cea6
isaacv4071 <isaacv4071@gmail.com>
2021-12-10 13:05:51 -0500

    Inico de la Actividad002-02

---
+++ corte2/Actividad002-02/Mites01.sql
@@ -0,0 +1,27 @@
+-- Database: Mites01
+
+CREATE TABLE alumnos (
+  id INTEGER NOT NULL,
+  nombre VARCHAR(30) NOT NULL,
+  apellido1 VARCHAR(50) NOT NULL,
+  apellido2 VARCHAR(50) NOT NULL,
+  nota REAL NOT NULL,
+  PRIMARY KEY (id)
+);
+
+insert into alumnos (id, nombre, apellido1, apellido2, nota) values (1, 'Mitchel', 'Lafranconi', 'Saipy', 4.5);
+insert into alumnos (id, nombre, apellido1, apellido2, nota) values (2, 'Jean', 'Waring', 'Bonicelli', 2.7);
+insert into alumnos (id, nombre, apellido1, apellido2, nota) values (3, 'Izak', 'Pilgrim', 'Van Brug', 1.3);
+insert into alumnos (id, nombre, apellido1, apellido2, nota) values (4, 'Russell', 'Bulfit', 'Simco', 1.0);
+insert into alumnos (id, nombre, apellido1, apellido2, nota) values (5, 'Ethel', 'Jeduch', 'McVity', 2.0);
+insert into alumnos (id, nombre, apellido1, apellido2, nota) values (6, 'Rourke', 'Elecum', 'Hansford', 2.9);
+insert into alumnos (id, nombre, apellido1, apellido2, nota) values (7, 'Fifine', 'Blackmoor', 'Tunder', 1.2);
+insert into alumnos (id, nombre, apellido1, apellido2, nota) values (8, 'Raina', 'Gahagan', 'Capponeer', 2.8);
+insert into alumnos (id, nombre, apellido1, apellido2, nota) values (9, 'Waylan', 'McCree', 'Birwhistle', 3.0);
+insert into alumnos (id, nombre, apellido1, apellido2, nota) values (10, 'Goldia', 'Frangleton', 'Primrose', 2.7);
+insert into alumnos (id, nombre, apellido1, apellido2, nota) values (11, 'Angelo', 'Doale', 'Brayshay', 1.1);
+insert into alumnos (id, nombre, apellido1, apellido2, nota) values (12, 'Tilda', 'Mountlow', 'MacLaren', 2.1);
+insert into alumnos (id, nombre, apellido1, apellido2, nota) values (13, 'Sullivan', 'D''orsay', 'Wadesworth', 2.7);
+insert into alumnos (id, nombre, apellido1, apellido2, nota) values (14, 'Carolina', 'Orring', 'Dinan', 1.1);
+insert into alumnos (id, nombre, apellido1, apellido2, nota) values (15, 'Jerrold', 'Marsden', 'Riglesford', 1.1);
+insert into alumnos (id, nombre, apellido1, apellido2, nota) values (16, 'Brok', 'Turl', 'Izzard', 4.1);
---
+++ corte2/Actividad002-02/Mites02.sql
@@ -0,0 +1,27 @@
+-- Database: Mites02
+
+CREATE TABLE alumnos (
+  id INTEGER NOT NULL,
+  nombre VARCHAR(30) NOT NULL,
+  apellido1 VARCHAR(50) NOT NULL,
+  apellido2 VARCHAR(50) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+insert into alumnos (id, nombre, apellido1, apellido2, email) values (1, 'Ugo', 'Pettis', 'Maplestone', 'umaplestone0@sfgate.com');
+insert into alumnos (id, nombre, apellido1, apellido2, email) values (2, 'Charline', 'Spraggon', 'Osgodby', 'cosgodby1@smh.com.au');
+insert into alumnos (id, nombre, apellido1, apellido2, email) values (3, 'Gay', 'Beetles', 'Duker', 'gduker2@tmall.com');
+insert into alumnos (id, nombre, apellido1, apellido2, email) values (4, 'Eddie', 'Fairbrother', 'Mottinelli', 'emottinelli3@seattletimes.com');
+insert into alumnos (id, nombre, apellido1, apellido2, email) values (5, 'Kippy', 'Gaythorpe', 'Speeding', 'kspeeding4@reference.com');
+insert into alumnos (id, nombre, apellido1, apellido2, email) values (6, 'Karlee', 'Grigoroni', 'Arson', 'karson5@gmpg.org');
+insert into alumnos (id, nombre, apellido1, apellido2, email) values (7, 'Bernete', 'Simoes', 'Durbin', 'bdurbin6@engadget.com');
+insert into alumnos (id, nombre, apellido1, apellido2, email) values (8, 'Abbie', 'Rahl', 'Stoddard', 'astoddard7@yahoo.com');
+insert into alumnos (id, nombre, apellido1, apellido2, email) values (9, 'Annora', 'Lerigo', 'Froome', 'afroome8@miitbeian.gov.cn');
+insert into alumnos (id, nombre, apellido1, apellido2, email) values (10, 'Ivar', 'Sclater', 'Rissen', 'irissen9@e-recht24.de');
+insert into alumnos (id, nombre, apellido1, apellido2, email) values (11, 'Deerdre', 'Largan', 'Salisbury', 'dsalisburya@delicious.com');
+insert into alumnos (id, nombre, apellido1, apellido2, email) values (12, 'Julian', 'Rehm', 'Heikkinen', 'jheikkinenb@china.com.cn');
+insert into alumnos (id, nombre, apellido1, apellido2, email) values (13, 'Ynes', 'Matteacci', 'Fieldstone', 'yfieldstonec@netlog.com');
+insert into alumnos (id, nombre, apellido1, apellido2, email) values (14, 'Robyn', 'Charley', 'Griffoen', 'rgriffoend@bing.com');
+insert into alumnos (id, nombre, apellido1, apellido2, email) values (15, 'Eartha', 'Haugh', 'Duiguid', 'eduiguide@nymag.com');
+insert into alumnos (id, nombre, apellido1, apellido2, email) values (16, 'Jewell', 'Bramford', 'Moring', 'jmoringf@reddit.com');
